d6be2ba9007f1752fa2be05f98be491a5ab2bb9e
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-22 18:55:53 +0300

    Added application services

---
+++ src/services/application.service.ts
@@ -0,0 +1,91 @@
+
+import { ApplicationStatus } from "../generated/prisma/enums"
+import { prisma } from "../lib/prisma.js"
+import { CreateApplicationInput, UpdateApplicationInput, UpdateApplicationStatusInput } from "../schemas/application.schemas"
+
+
+export const getAllApplications = async (userId: string, jobId: string) => {
+    const job = await prisma.job.findUnique({ where: { id: jobId } })
+    if (!job) {
+        throw { status: 404, message: "Job not found." }
+    }
+    if (job.clientId != userId) {
+        throw { status: 403, message: "Unauthorized." }
+    }
+    return await prisma.application.findMany({
+        where: { jobId: jobId }
+    })
+}
+export const getCurrentUserApplications = async (userId: string) => {
+    return await prisma.application.findMany({
+        where: { freelancerId: userId }
+    })
+}
+export const createApplication = async (userId: string, jobId: string, data: CreateApplicationInput) => {
+    const job = await prisma.job.findUnique({ where: { id: jobId } })
+    if (!job) {
+        throw { status: 404, message: "Job not found." }
+    }
+    if (job.clientId == userId) {
+        throw { status: 403, message: "You cannot apply to your own job listing." }
+    }
+    const existingApplication = await prisma.application.findFirst({ where: { freelancerId: userId } })
+    if (existingApplication) {
+        throw { status: 400, message: "You have already submitted an application to this job." }
+    }
+    return await prisma.application.create({
+        data: {
+            ...data,
+            jobId: jobId,
+            freelancerId: userId
+        }
+    })
+}
+export const editApplication = async (userId: string, id: string, data: UpdateApplicationInput) => {
+    const application = await prisma.application.findUnique({ where: { id } })
+    if (!application) {
+        throw { status: 404, message: "Application not found." }
+    }
+    if (userId != application.freelancerId) {
+        throw { status: 403, message: "Unauthorized." }
+    }
+    if (application.status == "PENDING") {
+        throw { status: 400, message: "Accepted, rejected or withdrawn applications cannot be editted." }
+    }
+    return await prisma.application.update({ where: { id }, data: data })
+}
+
+export const editApplicationStatus = async (userId: string, id: string, data: UpdateApplicationStatusInput) => {
+    const application = await prisma.application.findUnique({ where: { id }, include: { job: true } })
+    if (!application) {
+        throw { status: 404, message: "Application not found." }
+    }
+    const isClient = userId == application.job.clientId
+    const isFreelancer = userId == application.freelancerId
+
+    if (data.status == "WITHDRAWN") {
+        if (!isFreelancer) {
+            throw { status: 403, message: "Only freelancer can withdraw an application." }
+        }
+    }
+    else if (data.status == "ACCEPTED" || data.status == "REJECTED") {
+        if (!isClient) {
+            throw { status: 403, message: "Only client can accept or reject an application." }
+        }
+    }
+    else {
+        throw { status: 400, message: "Invalid status." }
+    }
+    return await prisma.application.update({ where: { id }, data: data })
+}
+
+export const deleteApplication = async (userId: string, id: string) => {
+    const application = await prisma.application.findUnique({ where: { id } })
+    if (!application) {
+        throw { status: 404, message: "Application not found." }
+    }
+    if (userId != application.freelancerId) {
+        throw { status: 403, message: "Unauthorized." }
+    }
+    return await prisma.application.delete({ where: { id } })
+}
