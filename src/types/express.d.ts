d334acdd43b43fa426d72484ffe92c94262b0545
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-22 19:05:46 +0300

    Added AuthUser to express request

---
+++ src/types/express.d.ts
@@ -0,0 +1,17 @@
+export interface AuthUser {
+    userId: string
+    email: string
+    role: string
+    permissions: string[]
+    tokenVersion: number
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: AuthUser
+        }
+    }
+}
+
+export { } 
