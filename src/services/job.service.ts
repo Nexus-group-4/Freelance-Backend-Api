51a4fab09e078e50f00fd6c36d06d506714b26fd
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-23 12:00:18 +0300

    Removed authentication and permission checks

--- src/services/job.service.ts
+++ src/services/job.service.ts
@@ -37,29 +37,14 @@ export const createJob = async (userId: string, data: CreateJobInput) => {
     })
 }
 export const editJob = async (
-    userId: string,
     jobId: string,
     data: UpdateJobInput
 ) => {
-    const job = await prisma.job.findUnique({ where: { id: jobId } })
-    if (!job) {
-        throw { status: 404, message: "Job not found." }
-    }
-    if (job.clientId != userId) {
-        throw { status: 403, message: "Unauthorized." }
-    }
     return await prisma.job.update({
         where: { id: jobId },
         data: data
     })
 }
-export const deleteJob = async (userId: string, jobId: string) => {
-    const job = await prisma.job.findUnique({ where: { id: jobId } })
-    if (!job) {
-        throw { status: 404, message: "Job not found." }
-    }
-    if (job.clientId != userId) {
-        throw { status: 403, message: "Unauthorized." }
-    }
+export const deleteJob = async (jobId: string) => {
     return await prisma.job.delete({ where: { id: jobId } })
 }
