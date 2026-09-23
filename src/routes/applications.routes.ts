e3d686284bb25e015198c1798edc4a2d61e936f4
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-23 14:07:31 +0300

    Used middlewares on routes

--- src/routes/applications.routes.ts
+++ src/routes/applications.routes.ts
@@ -1,14 +1,45 @@
 import * as applicationControllers from "../controller/application.controller.js"
 import { Router } from "express"
+import { requirePermission } from "../middleware/permission.middleware.js"
+import { checkApplicationOrJobOwnership, checkApplicationOwnership, checkJobOwnership } from "../middleware/ownership.middleware.js"
 
 const router = Router()
 
 
-router.get("/jobs/:jobId/applications", applicationControllers.getAllApplications)
-router.get("/applications/me", applicationControllers.getCurrentUserApplications)
-router.post("/jobs/:jobId/applications", applicationControllers.createApplication)
-router.put("/applications/:id", applicationControllers.editApplication)
-router.patch("/applications/:id/status", applicationControllers.editApplicationStatus)
-router.delete("/applications/:id", applicationControllers.deleteApplication)
+router.get(
+    "/jobs/:jobId/applications",
+    checkJobOwnership,
+    requirePermission("application"),
+    applicationControllers.getAllApplications
+)
+
+router.get(
+    "/applications/me",
+    requirePermission("application:read"),
+    applicationControllers.getCurrentUserApplications)
+
+router.post(
+    "/jobs/:jobId/applications",
+    requirePermission("application:create"),
+    applicationControllers.createApplication
+)
+router.put(
+    "/applications/:id",
+    requirePermission("application:update"),
+    checkApplicationOwnership,
+    applicationControllers.editApplication
+)
+router.patch(
+    "/applications/:id/status",
+    requirePermission("application:update"),
+    checkApplicationOrJobOwnership,
+    applicationControllers.editApplicationStatus
+)
+router.delete(
+    "/applications/:id",
+    requirePermission("application:delete"),
+    checkApplicationOwnership,
+    applicationControllers.deleteApplication
+)
 
 export default router
