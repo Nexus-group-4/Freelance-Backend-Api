95550fc73c65f0d43423ea5188987af56802dffc
Isaac-Habesha <thegreatestbaki@gmail.com>
2026-09-25 01:35:56 +0300

    adding the skill route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import categoryRoutes from "./routes/categories.routes.js";
 import userRoutes from "./routes/users.routes.js";
+import skillRoutes from "./routes/skills.routes.js";
+
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(express.json());
 
 app.use("/api/categories", categoryRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/skills", skillRoutes);
 
 export default app;
