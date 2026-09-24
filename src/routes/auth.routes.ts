41c953d5bc36d0241f61e6bb9ce15e9a2fd4daff
killshot-0 <eyosik55@gmail.com>
2026-09-24 21:35:17 +0300

    Fixed auth route and added Category route on Readme

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -8,6 +8,8 @@ authRouter.post('/register', controllers.registering);
 
 authRouter.post('/login', controllers.loging);
 
+authRouter.post('/refresh', controllers.refresh)
+
 authRouter.post('/logout', controllers.logout);
 
 authRouter.post('/logout-all', controllers.logoutAll);
