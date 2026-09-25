f72046f3ab066c3362bd1bf19efe4e5f48caf678
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-09-25 13:49:17 +0300

    Added contract ownership checking middleware

--- src/middleware/ownership.middleware.ts
+++ src/middleware/ownership.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { application, NextFunction, Request, Response } from "express";
 import { prisma } from "../lib/prisma.js"
 
 
@@ -7,19 +7,41 @@ export const checkJobOwnership = async (req: Request<{ id: string }>, res: Respo
         if (!req.user) {
             return res.status(401).json({ message: "Authentication required" })
         }
+
+        const applicationId = req.body.applicationId
         const userId = req.user!.userId
-        const jobId = req.params.id
+        let clientId: string | undefined
 
-        const job = await prisma.job.findUnique({
-            where: { id: jobId },
-            select: { clientId: true },
-        })
+        // This block is for checking job ownership for the contract routes, since there's no jobId in params for them
+        if (applicationId) {
+            const application = await prisma.application.findUnique({
+                where: { id: applicationId },
+                select: { job: { select: { clientId: true } } }
+            })
+            if (!application) {
+                return res.status(404).json({ message: "Application not found." })
+            }
 
-        if (!job) {
-            return res.status(404).json({ message: "Job not found." })
+            clientId = application.job.clientId
+
         }
+        // and this block is for the application routes, since they contain jobId in params
+        else {
+            const jobId = req.params.id
 
-        const isOwner = job.clientId == userId
+            const job = await prisma.job.findUnique({
+                where: { id: jobId },
+                select: { clientId: true },
+            })
+
+            if (!job) {
+                return res.status(404).json({ message: "Job not found." })
+            }
+
+            clientId = job.clientId
+
+        }
+        const isOwner = clientId == userId
         const isAdmin = req.user!.role == "ADMIN"
 
         if (!isOwner && !isAdmin) {
@@ -27,6 +49,7 @@ export const checkJobOwnership = async (req: Request<{ id: string }>, res: Respo
         }
 
         next()
+
     } catch (err) {
         next(err)
     }
@@ -88,6 +111,45 @@ export const checkApplicationOrJobOwnership = async (req: Request<{ id: string }
             return res.status(403).json({ message: "Unauthorized" })
         }
 
+        next()
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const checkContractOwnership = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+        if (!req.user) {
+            return res.status(401).json({ message: "Authentication required" })
+        }
+
+        const userId = req.user!.userId
+        const contractId = req.params.id
+        const contract = await prisma.contract.findUnique({
+            where: { id: contractId },
+            select: {
+                application: {
+                    select: {
+                        freelancerId: true,
+                        job: {
+                            select: { clientId: true }
+                        }
+                    }
+                }
+            }
+        })
+
+        if (!contract) {
+            return res.status(404).json({ message: "Contract not found" })
+        }
+
+        const isAdmin = req.user!.role == "ADMIN"
+        const isFreelancer = contract.application.freelancerId === userId
+        const isClient = contract.application.job.clientId === userId
+
+        if (!isFreelancer && !isClient && !isAdmin) {
+            return res.status(403).json({ message: "Unauthorized." })
+        }
         next()
     } catch (err) {
         next(err)
